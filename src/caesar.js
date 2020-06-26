31b17babaab71b258dc0895bd8d3118733678574
Andres Ruiz <andrsruiz@gmail.com>
2020-06-26 11:05:51 -0500

    Cipher upper case and lower case

--- src/caesar.js
+++ src/caesar.js
@@ -1,7 +1,15 @@
+// 97 - 122 lower case letters
+// 65 - 90 upper case letters
+
 const caesar = (word, key) => {
 	let cipher = '';
 	for(let i = 0; i < word.length; i++){
-		cipher += String.fromCharCode(word.charCodeAt(i) + key);
+		let charCode = word.charCodeAt(i);
+		if((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)){
+			cipher += String.fromCharCode(charCode + key);
+		}else{
+			cipher += word[i];
+		}
 	}
 	return cipher;
 }
