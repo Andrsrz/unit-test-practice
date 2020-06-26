e3f38d1da3f11ca8f9b4366c6620a4d489b8b2ce
Andres Ruiz <andrsruiz@gmail.com>
2020-06-26 10:21:48 -0500

    Cipher word

--- src/caesar.js
+++ src/caesar.js
@@ -1,5 +1,8 @@
-const caesar = (letter, key) => {
-	let cipher = String.fromCharCode(letter.charCodeAt(0) + key);
+const caesar = (word, key) => {
+	let cipher = '';
+	for(let i = 0; i < word.length; i++){
+		cipher += String.fromCharCode(word.charCodeAt(i) + key);
+	}
 	return cipher;
 }
 module.exports = caesar;
--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,5 +1,9 @@
 const caesar = require('../src/caesar');
 
-test('Caesar cipher letter', () => {
+test('Caesar cipher (1) letter', () => {
 	expect(caesar("a", 5)).toMatch("f");
+});
+
+test('Caesar cipher (2) word', () => {
+	expect(caesar("andres", 5)).toMatch("fsiwjx");
 });
